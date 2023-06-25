a980dffb30a7f292a13a2da9009b638ab849fc5b
Mario Glaucon <mglaucon@gmail.com>
2023-06-25 00:26:02 -0300

    Login
    Primeira Tela criada e em desenvolvimento.

---
+++ CRUD_V1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+    <main>
+        <div class="form">
+            <form action="login" method="post">
+                <label for="usuario">Usuário</label>
+                <input type="text" name="usuario" id="usuario">
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" id="senha">
+                <input type="submit" value="Entrar">
+            </form>
+        </div>
+    </main>
+</body>
+</html>
