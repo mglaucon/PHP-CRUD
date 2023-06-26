33252a48675b2a6e33e9a1188c52fee1fc415aae
Mario Glaucon <mglaucon@gmail.com>
2023-06-26 15:57:11 -0300

    Listar Painel
    Criado a lista no panel.php e criando o editar.

--- CRUD_V1/login.php
+++ CRUD_V1/login.php
@@ -2,12 +2,19 @@
 session_start();
 include('config.php');
 
-$sql = "SELECT * FROM usuarios WHERE nome='Mario' AND senha=MD5('12345')"
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
 
-if (empty($_POST['usuario']) || empty($_POST['senha'])):
+
+$sql = "SELECT nome,senha FROM usuarios WHERE nome='".$usuario."' AND senha=MD5('".$senha."')";
+
+$res = $conn->query($sql);
+$row = $res->fetch_object();
+
+if (!isset($row->nome) || !isset($row->senha)):
     $_SESSION['login_validator'] = 'Usuario ou senha inválidos!';
     header("Location: index.php");
 else:
     $_SESSION['login'] = $_POST['usuario'];
-    header("Location: painel.php");
+    header("Location: panel.php");
 endif;
--- CRUD_V1/panel.php
+++ CRUD_V1/panel.php
@@ -3,9 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>painel</title>
+    <title>Painel</title>
+    <style>
+        table{
+            width: 80vw;
+            text-align: center;
+            margin: auto;
+            border-collapse: collapse;
+            border: 1px solid lightgray;
+        }
+        th{
+            border: 1px solid white;
+        }
+        td{
+            border: 1px solid lightgray;
+            padding: 5px 15px;
+        }
+        thead{
+            background-color: lightgray;
+        }
+        table > tbody > tr:nth-child(1n):hover{
+            background-color: lightgray;
+            
+        }
+    </style>
 </head>
 <body>
-    
+    <?php 
+        include('config.php');
+        $sql = "SELECT id,nome,email,data_nasc FROM usuarios";
+        $res = $conn->query($sql);
+        $qtd = $res->num_rows;
+    ?>
+    <table>
+        <thead>
+            <th>#</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Data de Nascimento</th>
+            <th>Ação</th>
+        </thead>
+        <tbody>
+            <?php 
+                if($qtd > 0):
+                    while($row = $res->fetch_object()):
+            ?>
+            <tr>
+                <td><?=$row->id?></td>
+                <td><?=$row->nome?></td>
+                <td><?=$row->email?></td>
+                <td><?=$row->data_nasc?></td>
+                <td><a href="editar.php?id=<?=$row->id?>"><button>Editar</button></a> <a href="excluir.php?id=<?=$row->id?>"><button>Excluir</button></a></td>
+            </tr>
+            <?php 
+                    endwhile;
+                endif;
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
